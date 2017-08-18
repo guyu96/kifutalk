56217079a01bbd963683a3f386d1e6ba4a5dbf59
Guyu Fan <guyufan1996@gmail.com>
2017-08-18 17:47:39 +0800

    adjust kifus per page

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 BCRYPT_LOG_ROUNDS = 15
 SGF_FOLDER = '/home/guyu/kifutalk/app/static/assets/sgf'
 THUMBNAIL_FOLDER = '/home/guyu/kifutalk/app/static/assets/thumbnail'
-PERPAGE = 6
+PERPAGE = 5
 THUMBNAIL_SIZE = (512, 512)
 URL_TIMEOUT = 10 # seconds
