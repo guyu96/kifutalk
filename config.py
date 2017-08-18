c752724e1b8469cce569effa63a59ea5d728974e
Guyu Fan <guyufan1996@gmail.com>
2017-08-18 18:51:18 +0800

    store empty board thumbnail on server

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 BCRYPT_LOG_ROUNDS = 15
 SGF_FOLDER = '/home/guyu/kifutalk/app/static/assets/sgf'
 THUMBNAIL_FOLDER = '/home/guyu/kifutalk/app/static/assets/thumbnail'
+EMPTY_BOARD_DATAURL = '/home/guyu/kifutalk/app/static/assets/empty_board.dataurl'
 PERPAGE = 5
 THUMBNAIL_SIZE = (512, 512)
 URL_TIMEOUT = 10 # seconds
