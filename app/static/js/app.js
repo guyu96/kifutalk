356b9926ca1f9b024f11c04ef8cde13d2b7045c0
Guyu Fan <guyufan1996@gmail.com>
2017-09-03 23:26:56 -0400

    scroll comment into view when comment-id query param is specified

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,4 +1,5 @@
 var driver = new Driver(kifu.sgf);
+
 if (nodeID) {
   try {
     driver.navigateTo(parseInt(nodeID));
@@ -19,6 +20,28 @@ var controller = new Controller(
   bc,
   document.getElementById('control')
 );
+
+// if commentID is also available (nodeID must be available too)
+// scroll that comment into view
+if (nodeID && commentID) {
+  try {
+    // find the comment element
+    var commentElements = controller.html.commentList.childNodes;
+    var commentElement = null;
+    for (var i = 0; i < commentElements.length; i++) {
+      if (commentElements[i].getAttribute('comment-id') === commentID) {
+        commentElement = commentElements[i];
+        commentElement.classList.add('highlight');
+        break;
+      }
+    }
+    if (commentElement) {
+      commentElement.scrollIntoView();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+}
 
 if (edit === 'True') {
   controller.html.toggleEdit.click();
