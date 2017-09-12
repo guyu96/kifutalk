a9e3b1df6341738037bf63d2361ea3d6c3f126b3
Guyu Fan <guyufan1996@gmail.com>
2017-09-12 17:51:31 -0400

    add if statement in case there is no new notifications

--- app/static/js/notification.js
+++ app/static/js/notification.js
@@ -7,22 +7,24 @@ var markAsRead = function(notificationID) {
   xhr.send();
 }
 
-notificationHeader.addEventListener('click', function(e) {
-  if (notificationUL.children.length === 0) {
-    return;
-  }
-  if (notificationUL.style.display === 'none') {
-    notificationUL.style.display = 'block';
-    notificationHeader.style.background = '#254977';
-  } else {
-    notificationUL.style.display = 'none';
-    notificationHeader.style.background = '#355987';
-  }
-});
-
-for (var i = 0; i < notificationUL.children.length; i++) {
-  var notificationAnchor = notificationUL.children[i];
-  notificationAnchor.addEventListener('click', function(e) {
-    markAsRead(notificationAnchor.getAttribute('nid'));
+if (notificationHeader) {
+  notificationHeader.addEventListener('click', function(e) {
+    if (notificationUL.children.length === 0) {
+      return;
+    }
+    if (notificationUL.style.display === 'none') {
+      notificationUL.style.display = 'block';
+      notificationHeader.style.background = '#254977';
+    } else {
+      notificationUL.style.display = 'none';
+      notificationHeader.style.background = '#355987';
+    }
   });
+
+  for (var i = 0; i < notificationUL.children.length; i++) {
+    var notificationAnchor = notificationUL.children[i];
+    notificationAnchor.addEventListener('click', function(e) {
+      markAsRead(notificationAnchor.getAttribute('nid'));
+    });
+  }
 }
